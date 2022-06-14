7f9acef8cc32fd86091809f26e76b4973cb72580
hdcodesdev <91739271+hdcodesdev@users.noreply.github.com>
2022-06-14 18:49:39 +0100

    useRef hook, updated to use button click event

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import { useEffect, useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 import useRandomJoke from "./useRandomJoke";
 
 function App() {
-  // Custom Hook
-  const joke = useRandomJoke("Jim", "Bob");
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+
+  const [firstName, setFirstName] = useState("Harinder");
+  const [lastName, setLastName] = useState("Dulai");
+
+  const joke = useRandomJoke(firstName, lastName);
+
+  const generateJoke = (e) => {
+    e.preventDefault();
+    setFirstName(firstNameRef.current.value);
+    setLastName(lastNameRef.current.value);
+  };
 
   return (
     <div className="app">
       <center>
         <h1>The Joke Generator</h1>
         <h2>{joke}</h2>
-        <button>Generate Joke</button>
+
+        <form>
+          <input placeholder="first name" ref={firstNameRef} />
+          <input placeholder="last name" ref={lastNameRef} />
+          <button type="submit" onClick={generateJoke}>
+            Generate Joke
+          </button>
+        </form>
       </center>
     </div>
   );
